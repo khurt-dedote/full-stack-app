2cf475b9cc14cba30ffcf851ef28622d63b1b644
khurt-dedote <kkmdedote.student@ua.edu.ph>
2025-04-29 10:03:39 +0800

    first commit

---
+++ src/data.tsx
@@ -0,0 +1,26 @@
+export const sculptureList = [{
+  name: 'Wally Bayola',
+  artist: 'Eat Bulaga',
+  description: 'He was discovered while at a comedy bar, and became host of Eat Bulaga! beginning on May 3, 2000, his 28th birthday. He then formed a comedic duo with Jose Manalo.',
+  url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm1mKDOq4k-M0yP4UVp4dltddl2hMG2IZsgg&s',
+  alt: 'Wally Bayola Picture.'
+}, {
+  name: 'Boss Toyo',
+  artist: 'Geng Geng',
+  description: 'Boss Toyos real name is Jayson Luzadas. He is an adopted son who said he ran away from his adoptive parents when he was in Grade 6 upon discovering he was not their biological son.',
+  url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSMvFnxwTP_ogLsqZhF9vC6A1ScwuScKvN-w&s',
+  alt: 'Boss Toyo Picture.'
+}, {
+  name: 'Bong Revilla',
+  artist: 'Senator of the Philippines',
+  description: 'commonly known as Bong Revilla, is a Filipino actor, director, producer, television presenter and politician serving as a Senator since 2019, and previously from 2004 to 2016."',
+  url: 'https://images.squarespace-cdn.com/content/v1/5bd06baf01232c415484d342/f098ad2e-3fe9-42dc-b156-1231332c8dfe/br+pic.jpg',
+  alt: 'Bong Reviila Picture.'
+}, {
+  name: 'Chris Brown',
+  artist: 'American singer-songwriter and rapper',
+  description: 'Chris Brown (born May 5, 1989, Tappahannock, Virginia, U.S.) is an American rhythm-and-blues (R&B) singer, songwriter, and actor whose melodic voice and skilled dancing propelled him to fame, though his success was sometimes overshadowed by his tumultuous personal life.',
+  url: 'https://riffmagazine.com/wp-content/uploads/2024/08/DSC_1279.jpg',
+  alt: 'Chris Brown Picture.'
+}
+]
